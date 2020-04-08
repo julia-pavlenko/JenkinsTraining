5cc36b55d09bc33352e0e10a41db316fd9e845bd
julia-pavlenko <34808762+julia-pavlenko@users.noreply.github.com>
2020-04-08 16:39:28 +0300

    Some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! Some changes");
+        System.out.println("Hello World! Some changes2");
     }
 }
